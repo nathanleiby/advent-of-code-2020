652b839795b68a4b6a1c0ccacc8309419d7a3c09
Nathan Leiby <nathanleiby@gmail.com>
2020-12-06 13:56:58 -0800

    problem 4 - part 2

--- 4.jl
+++ 4.jl
@@ -1,18 +1,73 @@
-required_fields = [
-"byr",
-"iyr",
-"eyr",
-"hgt",
-"hcl",
-"ecl",
-"pid",
-# "cid", # confusing: this is described as a "required" field at first but then "optional" later
-]
+required_fields = Dict(
+# byr (Birth Year) - four digits; at least 1920 and at most 2002.
+"byr" => function (s::SubString)
+    x = parse(Int64, s)
+    return 1920 <= x && x <= 2002
+end,
+# # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+"iyr" => function (s::SubString)
+    x = parse(Int64, s)
+    return 2010 <= x && x <= 2020
+end,
+# # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+"eyr" => function (s::SubString)
+    x = parse(Int64, s)
+    return 2020 <= x && x <= 2030
+end,
+# # hgt (Height) - a number followed by either cm or in:
+#     # If cm, the number must be at least 150 and at most 193.
+#     # If in, the number must be at least 59 and at most 76.
+"hgt" => function (s::SubString)
+    x = parse(Int64, s[1:length(s) - 2])
+    unit = s[length(s) - 1:length(s)]
+    if unit == "cm"
+        # cm case
+        return 150 <= x && x <= 193
+    else
+        # in case
+        return 59 <= x && x <= 76
+    end
+end,
+# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+"hcl" => function (s::SubString)
+    # TODO: check for alphanumeric
+    return length(s) == 7 && s[1] == '#'
+end,
+# # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+"ecl" => function (s::SubString)
+    return s in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+end,
+# # pid (Passport ID) - a nine-digit number, including leading zeroes.
+"pid" => function (s::SubString)
+    # has 9 digits?
+    if ! (length(s) == 9)
+        return false
+    end
+    # is numeric?
+    try
+        parse(Int64, s)
+        return true
+    catch
+        return false
+    end
+end,
+# # "cid", # confusing: this is described as a "required" field at first but then "optional" later
+)
 
-function is_valid(o::Dict)
+
+
+
+function is_valid(passport::Dict)
     result = true
-    for k in required_fields
-        if ! haskey(o, k)
+    for k in keys(required_fields)
+        if ! haskey(passport, k)
+            result = false
+            break
+        end
+
+        rule = required_fields[k]
+        if ! rule(passport[k])
+            println("Failed rule ", k, " for passport ", passport)
             result = false
             break
         end
@@ -62,6 +117,7 @@ function run(fname)
     return out
 end
 
-@assert run("4ex.txt") == 2
+@assert run("4ex2.txt") == 0
+@assert run("4ex3.txt") == 4
 
 run("4.txt")
