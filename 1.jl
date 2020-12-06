5e9cf528871142c6eec885b1d9a1db463b15e044
Nathan Leiby <nathanleiby@gmail.com>
2020-12-05 20:04:11 -0800

    problem 1 - part 2

--- 1.jl
+++ 1.jl
@@ -14,7 +14,7 @@ numbers = map((x) -> parse(Int64, x), lines)
 # > ["this is a simple file containing", "text and numbers:", "43.3", "17"]
 close(f)
 
-function compute(d)
+function compute2(d)
     for i in 1:length(d)
         for j in i + 1:length(d)
             if d[i] + d[j] == 2020
@@ -25,6 +25,24 @@ function compute(d)
     return -1
 end
 
-println(compute(example))
+function compute3(d)
+    for i in 1:length(d)
+        for j in i + 1:length(d)
+            for k in j + 1:length(d)
+                if d[i] + d[j] + d[k] == 2020
+                    return d[i] * d[j] * d[k]
+                end
+            end
 
-println(compute(numbers))
+        end
+    end
+    return -1
+end
+
+
+println(compute2(example))
+println(compute3(example))
+
+println(compute2(numbers))
+println(compute3(numbers))
+
