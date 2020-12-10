63ec76fc61e13401c73c85e32706999e20530444
Nathan Leiby <nathanleiby@gmail.com>
2020-12-10 11:49:21 -0800

    problem 10 - part 2

--- 10.jl
+++ 10.jl
@@ -1,4 +1,8 @@
 using Test;
+using Combinatorics;
+# LEARNINGS:
+# - for run2: using Combinatorics + powerset(nums) ... is the right logic but WAAAAY too slow
+
 
 function run(fname)
     differences = Dict()
@@ -11,6 +15,7 @@ function run(fname)
     lines = readlines(f)
     nums = map((x) -> parse(Int, x), lines)
     sort!(nums)
+    println(nums)
 
     # START edge case (wall has joltage of 0)
     diff = nums[1] - 0
@@ -31,6 +36,82 @@ function run(fname)
     return result
 end
 
+function run2(fname)
+    f = open(fname, "r")
+    lines = readlines(f)
+    nums = map((x) -> parse(Int, x), lines)
+    sort!(nums)
+
+    valid_count = 0
+    min_val = 0
+    max_val = nums[length(nums)] + 3
+
+    nums = [min_val, nums..., max_val]
+
+    # NEW ALGO:
+    # - find all jumps of 3.
+    # - partition the list into sublists, separated by those jumps.
+    all_sets = []
+    current_set = [nums[1]]
+    for i in collect(2:length(nums))
+        diff = nums[i] - nums[i - 1]
+        if diff == 3
+            push!(all_sets, current_set)
+            current_set = [nums[i]]
+        else
+            push!(current_set, nums[i])
+        end
+    end
+    # add last set
+    push!(all_sets, current_set)
+
+    result = 1
+    for s in all_sets
+        # - run algo on those smaller sublists
+        # - multiply results
+        vs = valid_subsets(s)
+        println("S = ", s, " .. valid subsets = ", length(vs))
+        for vvs in vs
+            println("\t", vvs)
+        end
+        result *= length(vs)
+    end
+
+
+    println("[", fname, "] [part II] result = ", result)
+    return result
+end
+
+function valid_subsets(s)
+    if length(s) < 3
+        return 1
+    end
+
+    out = []
+    total = 0
+    inner_items = powerset(s[2:length(s) - 1])
+    for ii in inner_items
+        vs = [first(s), ii..., last(s)]
+        if is_valid(vs)
+            push!(out, vs)
+        end
+    end
+    return out
+end
+
+function is_valid(l)
+    for i in 2:length(l)
+        if (l[i] - l[i - 1]) > 3
+            return false
+        end
+    end
+    return true
+end
+
 @test run("10ex1.txt") == 7 * 5
 @test run("10ex2.txt") == 22 * 10
 run("10.txt")
+
+@test run2("10ex1.txt") == 8
+@test run2("10ex2.txt") == 19208
+run2("10.txt")
