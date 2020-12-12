b4d33c7c068a9d5947158922e2a14d49e4d1d625
Nathan Leiby <nathanleiby@gmail.com>
2020-12-11 22:15:59 -0800

    problem 12 - part 2

--- 12.jl
+++ 12.jl
@@ -55,5 +55,69 @@ function run(fname)
     return result
 end
 
+function reflect(start_pos, degrees)
+    deg = mod(degrees, 360)
+
+    if deg == 0
+        return start_pos
+    elseif deg == 90
+        # [1,2] .. R90  (L270) .. [2,-1]  # swap and -1 the second element
+        return [start_pos[2], -1 * start_pos[1]]
+    elseif deg == 180
+        # [1,2] .. R180 (L180) .. [-1,-2] # -1 both elements
+        return start_pos .* -1
+    elseif deg == 270
+        # [1,2] .. R270 (L90)  .. [-2,1]  # swap and -1 the first element
+        return [-1 * start_pos[2], start_pos[1]]
+    else
+        throw("invalid input: degrees = " * degrees)
+    end
+end
+
+
+function run2(fname)
+    f = open(fname, "r")
+    lines = readlines(f)
+
+    pos = [10, 1] # waypoint pos
+    ship_pos = [0, 0] # ship position
+    println(pos)
+    for l in lines
+        command = l[1]
+        number = parse(Int, l[2:length(l)])
+        println("cmd=", command, " num=", number)
+
+        if command == 'N'
+            pos = move(pos, 0, number)
+        elseif command == 'S'
+            pos = move(pos, 180, number)
+        elseif command == 'E'
+            pos = move(pos, 90, number)
+        elseif command == 'W'
+            pos = move(pos, 270, number)
+        elseif command == 'L'
+            pos = reflect(pos, -1 * number)
+        elseif command == 'R'
+            pos = reflect(pos, number)
+        elseif command == 'F'
+            # Move the ship
+            for i in 1:number
+                ship_pos += pos
+            end
+        end
+
+        println("ship_pos: ", ship_pos, " waypoint_pos: ", pos)
+    end
+
+    result = abs(ship_pos[1]) + abs(ship_pos[2])
+    println("[part 2] File = ", fname, " ... Result = ", result)
+
+    return result
+end
+
+
 @test run("12ex.txt") == 25
 run("12.txt")
+
+@test run2("12ex.txt") == 286
+run2("12.txt")
