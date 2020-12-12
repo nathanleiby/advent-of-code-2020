6ed96fd371c3d9b2024a20fd881c35e4e1ed0fae
Nathan Leiby <nathanleiby@gmail.com>
2020-12-11 21:57:03 -0800

    problem 12 - part 1

---
+++ 12.jl
@@ -0,0 +1,59 @@
+using Test;
+
+direction_to_xy = Dict(
+    0 => [0, 1], # north
+    90 => [1, 0], # east
+    180 => [0, -1], # south
+    270 => [-1, 0], # west
+)
+
+function turn(start_direction, degrees)
+    return mod(start_direction + degrees, 360)
+end
+
+function move(start_pos, direction, distance)
+    m = direction_to_xy[direction] .* distance
+    return start_pos + m
+end
+
+function run(fname)
+    f = open(fname, "r")
+    lines = readlines(f)
+
+    pos = [0, 0]
+    direction = 90 # East
+    println(pos)
+    for l in lines
+        command = l[1]
+        number = parse(Int, l[2:length(l)])
+        println("cmd=", command, " num=", number)
+
+        # Action N means to move north by the given value.
+        mov_dir = direction
+        if command == 'N'
+            pos = move(pos, 0, number)
+        elseif command == 'S'
+            pos = move(pos, 180, number)
+        elseif command == 'E'
+            pos = move(pos, 90, number)
+        elseif command == 'W'
+            pos = move(pos, 270, number)
+        elseif command == 'L'
+            direction = turn(direction, -1 * number)
+        elseif command == 'R'
+            direction = turn(direction, number)
+        elseif command == 'F'
+            pos = move(pos, direction, number)
+        end
+
+        println("pos: ", pos, " facing: ", direction_to_xy[direction])
+    end
+
+    result = abs(pos[1]) + abs(pos[2])
+    println("[part 1] File = ", fname, " ... Result = ", result)
+
+    return result
+end
+
+@test run("12ex.txt") == 25
+run("12.txt")
